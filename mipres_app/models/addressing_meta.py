ef475b487dd53b6bfe5341b414e271ae349797f7
Andy Fernandez <afgarabote@gmail.com>
2020-10-06 16:36:44 -0300

    skip store dates

--- mipres_app/models/addressing_meta.py
+++ mipres_app/models/addressing_meta.py
@@ -1,10 +1,18 @@
 from mipres_app.mipres import db_session, socketio
 from datetime import datetime, timedelta
-
+from flask_mongoalchemy import BaseQuery
 from mipres_app.models.addressing import Addressing
 
 
+class AddressingMetaQuery(BaseQuery):
+    def get_by_date(self, date):
+        end = date.replace(hour=23, minute=59)
+        return self.filter(self.type.document_date >= date, self.type.document_date <= end)
+
+
 class AddressingMeta(db_session.Document):
+    query_class = AddressingMetaQuery
+
     exec_date = db_session.DateTimeField()
     document_date = db_session.DateTimeField()
     observation = db_session.StringField()
@@ -35,8 +43,10 @@ class AddressingMeta(db_session.Document):
 
         while start_date <= end_date:
             days_left = (end_date - start_date).days
-            socketio.emit('addressing', dict(percent=100 - days_left * 100 / days, user=identity.get('nit')))
+            socketio.emit('addressing', dict(percent=round(100 - days_left * 100 / days), user=identity.get('nit')))
 
-            response = generate_token(identity.get('nit'), identity.get('token_pres'), start_date.strftime("%Y-%m-%d"))
-            AddressingMeta.save_document(start_date, response, len(response))
+            if AddressingMeta.query.get_by_date(start_date).first() is None:
+                response = generate_token(identity.get('nit'), identity.get('token_pres'), start_date.strftime("%Y-%m-%d"))
+                AddressingMeta.save_document(start_date, response, len(response))
+
             start_date += delta
