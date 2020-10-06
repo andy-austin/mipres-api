77bf5e42af1edd90318ce3b6ff30c297506b0e3c
Andy Fernandez <afgarabote@gmail.com>
2020-10-06 19:22:06 -0300

    change entrypoint.sh

--- k8s/entrypoint.sh
+++ k8s/entrypoint.sh
@@ -3,7 +3,7 @@
 exec gunicorn mipres_app.wsgi:app \
     --pid mipres-api.pid \
     --name docker_mipres_app \
-    --bind 0.0.0.0 \
+    --bind 0.0.0.0:80 \
     --workers 5 \
     --log-level=debug \
     --timeout 60
