f000592f2900bea02c43d7c54d91fb450bfe22db
Andy Fernandez <afgarabote@gmail.com>
2020-10-06 22:02:25 -0300

    bind to 0.0.0.0

--- k8s/entrypoint.sh
+++ k8s/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-exec gunicorn -k geventwebsocket.gunicorn.workers.GeventWebSocketWorker -w 1 mipres_app.wsgi:app
+exec gunicorn -k geventwebsocket.gunicorn.workers.GeventWebSocketWorker -w 1 --bind 0.0.0.0 mipres_app.wsgi:app
 "$@"
